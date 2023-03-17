c83fc3c32b2b1e16913b5b746f1d5e695d732a11
Marc Geraerts <marc.geraerts@uhasselt.be>
2023-03-17 07:58:19 +0100

    added yaml

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, Blueprint
 import tagdb
+import os
 
 print('')
 print(' **********************************************************')
@@ -12,19 +13,30 @@ print('')
 
 app = Flask(__name__)
 app.secret_key = "!g$FRrWwkqtCZfrsptyYWwBb*"
+
 db = tagdb.tagdb()
 db.create_db()
 
-
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'tagDB server'
 
 @app.route('/log', methods=['GET'])
 def log():
-    tagIDMD5 = request.args.get('hsh')
-    result = db.log_tag(tagIDMD5)
+    tagMD5 = request.args.get('hsh')
+    deviceMAC = request.args.get('mac')
+    result = db.log_tag(tagMD5, deviceMAC)
     return result
 
 
-app.run(host='0.0.0.0', port=81)
+@app.route ('/isfile/')
+def test3():
+    fname = os.getcwd() + os.path.sep + 'database' + os.path.sep + 'tagdb.db'
+    return (str(os.path.isfile(fname)))
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app.
+    app.run(host='127.0.0.1', port=81, debug=True)
+# [END gae_flex_quickstart]
--- tagdb.py
+++ tagdb.py
@@ -1,9 +1,7 @@
 import sqlite3
 import os
 import platform
-import calendar
 from datetime import datetime
-
 
 def dict_factory(cursor, row):
     d = {}
@@ -88,26 +86,82 @@ class tagdb(object):
         conn.close()
         return
 
-    def tagExists(self, tagMD5):
-
+    def deviceExists(self, deviceMAC):
         conn = sqlite3.connect(self.db_full_f_name)
-        conn.row_factory = dict_factory
         c = conn.cursor()
-        sql_string = "SELECT * FROM tagIDs WHERE tagID = '" + tagMD5 + "'"
+        sql_string = "SELECT ID FROM devices WHERE device_mac = '" + deviceMAC + "'"
         c.execute(sql_string)
         data = c.fetchall()
         conn.close()
-        return data
-
-    def log_tag(self, tagMD5):
-
-        ti = self.tagExists(tagMD5)
-        if ti:
-            return 'Tag in db'
+        if len(data) == 0:
+            return 0
         else:
-            return 'Not in db'
+            return data[0][0]
 
+    def insertDevice(self, deviceMAC):
 
+        current_date = datetime.now()
+        datenum = current_date.strftime("%Y%m%d%H%M%S")
+
+        sql_string = 'INSERT INTO devices (device_name, device_mac, device_entry_date)' \
+                     'VALUES ("", "' + deviceMAC + '",' + datenum + ')'
+        self.execute(sql_string)
+
+        sql_string = "SELECT  ID FROM devices ORDER BY ID DESC LIMIT 1"
+        conn = sqlite3.connect(self.db_full_f_name)
+        c = conn.cursor()
+        c.execute(sql_string)
+        data = c.fetchall()
+        conn.close()
+        lastID = data[0]
+        return lastID[0]
+
+    def tagExists(self, tagMD5):
+        conn = sqlite3.connect(self.db_full_f_name)
+        c = conn.cursor()
+        sql_string = "SELECT ID FROM tagIDs WHERE tagMD5 = '" + tagMD5 + "'"
+        c.execute(sql_string)
+        data = c.fetchall()
+        conn.close()
+        if len(data) == 0:
+            return 0
+        else:
+            return data[0][0]
+
+    def insertTag(self, tagMD5):
+
+        current_date = datetime.now()
+        datenum = current_date.strftime("%Y%m%d%H%M%S")
+
+        sql_string = 'INSERT INTO tagIDs (tagMD5, entry_date, person_id)' \
+                     'VALUES ("' + tagMD5 + '",' + datenum + ',-1)'
+        self.execute(sql_string)
+
+        sql_string = "SELECT  ID FROM tagIDs ORDER BY ID DESC LIMIT 1"
+        conn = sqlite3.connect(self.db_full_f_name)
+        c = conn.cursor()
+        c.execute(sql_string)
+        data = c.fetchall()
+        conn.close()
+        lastID = data[0]
+        return lastID[0]
+
+    def log_tag(self, tagMD5, deviceMAC):
+
+        tagID = self.tagExists(tagMD5)
+        if not tagID:
+            tagID = self.insertTag(tagMD5)
+
+        deviceID = self.deviceExists(deviceMAC)
+        if not deviceID:
+            deviceID = self.insertDevice(deviceMAC)
+
+        current_date = datetime.now()
+        datenum = current_date.strftime("%Y%m%d%H%M%S")
+        sql_string2 = 'INSERT INTO taglogs (tag_id, tag_timestamp, tag_device_ID' \
+                     ') VALUES (' +  str(tagID) + ',' + datenum + ',' +  str(deviceID) + ')'
+        self.execute(sql_string2)
+        return 'http200OK'
 
     def create_db(self):
         # ********************************************************
@@ -115,7 +169,7 @@ class tagdb(object):
 
         self.create_table(table_name)
 
-        columns = ["tagID TEXT DEFAULT ''", "entry_date INTEGER", "person_id INTEGER"]
+        columns = ["tagMD5 TEXT DEFAULT ''", "entry_date INTEGER", "person_id INTEGER"]
 
         self.insert_columns(table_name, columns)
 
@@ -134,8 +188,21 @@ class tagdb(object):
         table_name = 'taglogs'
 
         self.create_table(table_name)
-        columns = ["tag_id INTEGER DEFAULT ''",
+        columns = ["tag_ID INTEGER DEFAULT ''",
                    "tag_timestamp INTEGER DEFAULT ''",
-                   "tag_device TEXT DEFAULT ''"]
+                   "tag_lat INTEGER DEFAULT ''",
+                   "tag_lon INTEGER DEFAULT ''",
+                   "tag_device_ID TEXT DEFAULT ''"]
+
+        self.insert_columns(table_name, columns)
+
+        # ********************************************************
+        table_name = 'devices'
+
+        self.create_table(table_name)
+        columns = ["device_name TEXT DEFAULT ''",
+                   "device_mac TEXT DEFAULT ''",
+                   "device_is_fixed INTEGER DEFAULT -1",
+                   "device_entry_date INTEGER DEFAULT ''"]
 
         self.insert_columns(table_name, columns)
